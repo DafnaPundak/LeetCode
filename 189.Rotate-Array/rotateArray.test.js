486543512d2b3517fec3427f3fa07fe3323641a2
Dafna Pundak <dafna.m88@gmail.com>
2020-11-15 00:18:14 +0200

    solution accepted

--- 189.Rotate-Array/rotateArray.test.js
+++ 189.Rotate-Array/rotateArray.test.js
@@ -1,13 +1,21 @@
-const rotate = require("./rotateArray")
+const rotate = require("./rotateArray");
 
-test("", ()=> {
-    expect(rotate([], 0)).toEqual([])
-})
+test("", () => {
+  expect(rotate([], 0)).toEqual([]);
+});
 
-test("", ()=> {
-    expect(rotate([1,2], 0)).toEqual([1,2])
-})
+test("", () => {
+  expect(rotate([1, 2], 0)).toEqual([1, 2]);
+});
 
-test("", ()=> {
-    expect(rotate([1,2], 1)).toEqual([2,1])
-})
+test("", () => {
+  expect(rotate([1, 2], 1)).toEqual([2, 1]);
+});
+
+test("", () => {
+  expect(rotate([1, 2, 4, 0], 1)).toEqual([0, 1, 2, 4]);
+});
+
+test("", () => {
+  expect(rotate([1, 2, 3, 4, 5, 6, 7], 3)).toEqual([5, 6, 7, 1, 2, 3, 4]);
+});
