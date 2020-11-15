819ad5aac235f1cc6bfbab63fc75ff46a32bfafe
Dafna Pundak <dafna.m88@gmail.com>
2020-11-15 15:03:06 +0200

    solution accepted

--- 344.Reverse-String/reverseString.test.js
+++ 344.Reverse-String/reverseString.test.js
@@ -1,13 +1,21 @@
-const reverseString = require("./reverseString")
+const reverseString = require("./reverseString");
 
 test("", () => {
-    expect(reverseString([])).toEqual([])
-})
+  expect(reverseString([])).toEqual([]);
+});
 
 test("", () => {
-    expect(reverseString(["h", "i"])).toEqual(["i", "h"])
-})
+  expect(reverseString(["h", "i"])).toEqual(["i", "h"]);
+});
 
 test("", () => {
-    expect(reverseString(["d", "o", "g"])).toEqual(["g", "o", "d"])
-})
+  expect(reverseString(["d", "o", "g"])).toEqual(["g", "o", "d"]);
+});
+
+test("", () => {
+  expect(reverseString(["d", "o", "g", "!"])).toEqual(["!", "g", "o", "d"]);
+});
+
+test("", () => {
+    expect(reverseString(["h", "e", "l", "l", "o"])).toEqual(["o", "l", "l", "e", "h"]);
+  });
