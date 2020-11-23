5dfcfe116c9fd4a379a6ac51fb4fa451659c9c68
Dafna Pundak <dafna.m88@gmail.com>
2020-11-23 12:14:56 +0200

    solution accepted

--- 37.Reverse-Integer/reverseInteger.js
+++ 37.Reverse-Integer/reverseInteger.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 
-var reverse = function (x) {
+var reverseOld = function (x) {
   let xArray = (x + "").split("");
   let newArray = [];
 
@@ -20,11 +20,22 @@ var reverse = function (x) {
     for (let i = xArray.length - 1; i >= 1; i--) {
       newArray.push(xArray[i]);
     }
-    if (Number("-"+Number(newArray.join(""))) < -(2 ** 31)) {
+    if (Number("-" + Number(newArray.join(""))) < -(2 ** 31)) {
       return 0;
     } else {
-      return Number("-"+Number(newArray.join("")))
+      return Number("-" + Number(newArray.join("")));
     }
+  } else {
+    return 0;
+  }
+};
+
+var reverse = function (x) {
+  let reversed = Number(Math.abs(x).toString().split("").reverse().join(""));
+  if (x > 0 && reversed < 2 ** 31 - 1) {
+    return reversed;
+  } else if (x < 0 && Number("-" + reversed) > -(2 ** 31)) {
+    return Number("-" + reversed);
   } else {
     return 0;
   }
